82186b9cd52c356fa1eb712ca74cd658945ecf1c
ryancrumble <ryancrummypl@gmail.com>
2019-07-30 15:13:00 +1000

    handled the auth part

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://admin:admin1234@storybooks-dev-hg08x.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:
+    '847421124922-bkhblatsf022djtlsihcq9sgg5j7v3tj.apps.googleusercontent.com',
+  googleClientSecret: 'UZdCf0UvhJYXJ4OPOSiNn0_D'
+};
