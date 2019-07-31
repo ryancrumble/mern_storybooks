ef7dca6feedfd2297309255fcf185829fb343767
ryancrumble <ryancrummypl@gmail.com>
2019-07-31 13:05:39 +1000

    started UI

--- acompare.js
+++ acompare.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const passport = require('passport');
 
 // Load User Model
 require('./models/User');
@@ -12,54 +11,30 @@ require('./models/User');
 require('./config/passport')(passport);
 
 // Load Routes
-const index = require('./routes/index');
 const auth = require('./routes/auth');
 
 // Load Keys
 const keys = require('./config/keys');
 
-// Map global promises
+// Map Global Promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
 mongoose
-  .connect(keys.mongoURI, {
-    useNewUrlParser: true
-  })
-  .then(() => console.log('MongoDB Connected'))
+  .connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 const app = express();
 
-// Handlebars Middleware
-app.engine(
-  'handlebars',
-  exphbs({
-    defaultLayout: 'main'
-  })
-);
-app.set('view engine', 'handlebars');
-
-app.use(cookieParser());
-app.use(
-  session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-  })
-);
+app.get('/', (req, res) => {
+  res.send('It works!');
+});
 
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Set global vars
-app.use((req, res, next) => {
-  res.locals.user = req.user || null;
-  next();
-});
-
 // Use Routes
-app.use('/', index);
 app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
